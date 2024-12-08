d439fea308b9340dfad414b1f30dbe8163852d7f
abreuitalo <italo.ab.leite@gmail.com>
2024-12-08 16:26:22 -0300

    simbolos

---
+++ ruby-basico/simbolos.rb
@@ -0,0 +1,10 @@
+# simbolos (identificado) || (string estática)
+
+# implementada pelo ruby, para trabalhar com json
+
+{ "a" => "123aaa"}; # normal hash
+{ :a => "123aaa"}; # symbol
+{ a: "123aaa"}; # recomended
+
+# convert string for symbol
+"asd".to_sym
