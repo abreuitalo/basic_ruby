42844e04a2d280a73a792c81e59112b7dbee0e27
abreuitalo <italo.ab.leite@gmail.com>
2024-12-08 16:01:28 -0300

    vetores_array

---
+++ ruby-basico/vetores_array.rb
@@ -0,0 +1,18 @@
+v = []
+
+# add end
+v.push(0);
+v.push(1);
+v.push(2);
+
+# delete any value
+v.delete(1)
+
+# add initial
+v.unshift(30)
+
+# delete initial value
+v.shift()
+
+# delete end value
+v.pop()
