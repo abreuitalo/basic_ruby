61170bd6fb6e40093afd8ce43875e6edd147bb3d
abreuitalo <italo.ab.leite@gmail.com>
2024-12-08 15:51:33 -0300

    estruturas_repeticao

---
+++ ruby-basico/estruturas_repeticao.rb
@@ -0,0 +1,16 @@
+count = 1
+
+# while (enquanto for verdadeiro)
+while (count <= 10)
+  puts "italo -> #{count}";
+  count += 1;
+end
+
+# 11
+# until (enquanto for falso)
+until (count < 1)
+  puts "Sou Falso - #{count}"
+  count -= 1;
+end
+
+puts "Fim!"
