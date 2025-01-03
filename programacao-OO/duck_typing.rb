fcc87a2a6fd107695f37d62cce1de7389df65e72
abreuitalo <italo.ab.leite@gmail.com>
2025-01-02 23:26:11 -0300

    duck_typing

---
+++ programacao-OO/duck_typing.rb
@@ -0,0 +1,25 @@
+class Pato
+  def quack
+    "Quack! Quack!"
+  end
+end
+
+class PatoDoente
+  def quack
+    "Queeeck..."
+  end
+end
+
+class Pessoa
+  def apertar_o_pato(pato)
+    pato.quack
+  end
+end
+
+p1 = Pato.new
+p2 = PatoDoente.new
+
+pessoa = Pessoa.new
+
+puts pessoa.apertar_o_pato(p1)
+puts pessoa.apertar_o_pato(p2)
