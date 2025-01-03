b879b6d1ab493ef4fbfe06cbb1f02de2855c0dae
abreuitalo <italo.ab.leite@gmail.com>
2025-01-02 22:05:23 -0300

    instancia_classe_metodos

---
+++ programacao-OO/instancia_classe.rb
@@ -0,0 +1,14 @@
+class Teste
+  def ola # Metodo instancia
+    "Olá!"
+  end
+
+  def self.hello # Metodo de classe
+    "Hello!"
+  end
+end
+
+# obj = Teste.new
+# puts obj.ola
+
+puts Teste.hello
