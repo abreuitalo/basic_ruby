bf2d3c7460f3c85e27c6e63af27fd83dbce60073
abreuitalo <italo.ab.leite@gmail.com>
2025-01-04 20:10:22 -0300

    ARGV - Argument Vector

--- trabalhando_com_arquivos/app.rb
+++ trabalhando_com_arquivos/app.rb
@@ -1,11 +1,15 @@
-# Leitura
-File.open('teste.txt', 'r') do  |arq|
-  while line = arq.gets
-    puts line
+if ARGV.size > 0
+  # Leitura
+  File.open(ARGV[0], 'r') do  |arq|
+    while line = arq.gets
+      puts line
+    end
   end
+else
+  puts "Você deve informar o nome do arquivo. Ex: ruby app.rb teste.txt"
 end
 
 # Escrita
-File.open('./trabalhando_com_arquivos/teste2.txt', 'w') do |arq|
-  arq.puts "teste2"
-end
+# File.open('./trabalhando_com_arquivos/teste2.txt', 'w') do |arq|
+#   arq.puts "teste2"
+# end
